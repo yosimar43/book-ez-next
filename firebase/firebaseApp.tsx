96448f3311d5f3015ba01ce68ee2a2643a6526cf
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-09-15 00:04:50 -0500

    connect with firebase

--- firebase/firebaseApp.tsx
+++ firebase/firebaseApp.tsx
@@ -1,13 +1,6 @@
-import firebase from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export default function firebaseApp() {
   const firebaseConfig = {
     apiKey: "AIzaSyB82G51R_SWwou-3cUFv9s4SgTqVKyjITg",
@@ -22,4 +15,3 @@ export default function firebaseApp() {
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 }
-
