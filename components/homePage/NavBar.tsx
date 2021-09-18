fed559e2c2becd64ce9cfe9f3ffb5e015fbf574d
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-09-17 21:54:08 -0500

    feture(homePage navar)add write effect with typed.js

--- components/homePage/NavBar.tsx
+++ components/homePage/NavBar.tsx
@@ -1,13 +1,32 @@
-import Image from "next/image";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
-import favicon from "/public/educationtraining.png";
 
 export const NavBar: NextPage = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Hola", "¿Eres nuevo?", "Bienvenido a", "Book-ez"], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 600,
+      typeSpeed: 150,
+      backSpeed: 100,
+      backDelay: 100,
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <nav className="flex  justify-between pt-3 px-4 shadow-md">
       <div className="flex justify-center items-center p-4">
-        <p className="text-blue-400 text-4xl font-logo -rotate-3">book-ez</p>
+        <p className="text-blue-400 text-4xl font-logo -rotate-3" ref={el}>
+          hola
+        </p>
       </div>
       <div className="flex justify-evenly py-2 items-center font-quickSand">
         <Link href="/login">
