a1c0b97e15d33e3c033cdd2bd41b37a266b2dbb7
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-09-19 13:07:37 -0500

    ui(homePage):Add styeles to FeaturesAppDescription component

--- components/homePage/FeaturesAppDescriptions.tsx
+++ components/homePage/FeaturesAppDescriptions.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 interface CardFeatures {
   cardContent: {
+    reverse: boolean;
     title: string;
     information: string;
     image: any;
@@ -19,19 +20,25 @@ interface CardFeatures {
 export const FeaturesAppDescriptions: NextPage<CardFeatures> = ({
   cardContent,
 }) => {
-  let { image, alt, title, information, link } = cardContent;
+  let { reverse, image, alt, title, information, link } = cardContent;
+
+  let isReverse = reverse ? "flex-row-reverse" : "flex-row";
 
   return (
-    <div className="w-11/12 p-4 flex justify-evenly rounded-lg items-stretch shadow-md   h-auto  mx-auto mt-8">
-      <div className="w-96 h-96 rounded-3xl">
+    <div
+      className={`w-11/12 p-4 flex justify-evenly rounded-lg items-stretch shadow-md h-auto mx-auto my-8  ${isReverse}`}
+    >
+      <div className="w-96 h-96 rounded-3xl overflow-hidden">
         <Image src={image} alt={alt} layout="responsive" />
       </div>
-      <div className="flex flex-col py-8 justify-center">
-        <h2 className="font-raleway text-4xl text-blue-600 ">{title}</h2>
-        <p className=" font-quickSand">{information}</p>
+      <div className="flex flex-col py-8 justify-evenly">
+        <h2 className="font-raleway text-5xl text-blue-600 py-4">{title}</h2>
+        <p className=" font-quickSand text-2xl">{information}</p>
         {link ? (
           <Link href={link.linkRef}>
-            <a>{link.linkText}</a>
+            <a className="max-w-max min-w-min bg-blue-400 rounded-3xl font-quickSand px-2 py-2 transition duration-500 hover:bg-blue-600 hover:text-white">
+              {link.linkText}
+            </a>
           </Link>
         ) : null}
       </div>
