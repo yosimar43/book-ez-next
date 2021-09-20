0cadbf2d42627cc4b2a8842db8e5573bf393e622
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-09-20 14:39:11 -0500

    ui(homePage):add animation to FeatureAppDescriptions

--- components/homePage/FeaturesAppDescriptions.tsx
+++ components/homePage/FeaturesAppDescriptions.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import type { NextPage } from "next";
 import Image from "next/image";
+import gsap from "gsap";
+import stackedSteps from "../../public/stacked-steps-haikei.svg";
 
 interface CardFeatures {
   cardContent: {
@@ -20,18 +22,60 @@ interface CardFeatures {
 export const FeaturesAppDescriptions: NextPage<CardFeatures> = ({
   cardContent,
 }) => {
+  const [isHoverCard, setIsHoverCard] = useState<boolean>(false);
+
+  useEffect(() => {
+    const initCardAnimation = () => {
+      gsap
+        .timeline()
+        .to(".svg", {
+          x: "100%",
+          transitionDuration: 2,
+          duration: 2,
+        })
+        .fromTo("img", { opacity: 0 }, { delay: 1, opacity: 1, duration: 0.5 })
+        .fromTo(
+          ".text",
+          {
+            scale: 0,
+          },
+          { scale: 1, duration: 1 }
+        );
+    };
+    if (isHoverCard) {
+      initCardAnimation();
+      return;
+    } else {
+      gsap.to(".svg", {
+        transitionTimingFunction: "ease",
+        x: 0,
+        duration: 1,
+      });
+    }
+  }, [isHoverCard]);
+
   let { reverse, image, alt, title, information, link } = cardContent;
 
   let isReverse = reverse ? "flex-row-reverse" : "flex-row";
 
   return (
     <div
-      className={`w-11/12 p-4 flex flex-wrap justify-evenly rounded-lg items-stretch shadow-md h-auto mx-auto my-8  ${isReverse}`}
+      onMouseEnter={() => setIsHoverCard(true)}
+      onMouseLeave={() => setIsHoverCard(false)}
+      className={`w-11/12 relative p-4 flex flex-wrap justify-evenly rounded-lg items-stretch shadow-md h-auto mx-auto my-8  ${isReverse} overflow-hidden`}
     >
-      <div className="w-96 h-96 rounded-3xl overflow-hidden">
+      <div className="svg w-full h-full z-10 bg-red-50 absolute ">
+        <Image
+          src={stackedSteps}
+          alt="Icono vectorial escaleras"
+          layout="responsive"
+        />
+      </div>
+
+      <div className="w-96 h-96 rounded-3xl overflow-hidden img ">
         <Image src={image} alt={alt} layout="responsive" />
       </div>
-      <div className="flex flex-col py-8 justify-evenly">
+      <div className="flex flex-col py-8 justify-evenly text">
         <h2 className="font-raleway text-5xl text-blue-600 py-4 ">{title}</h2>
         <p className=" font-quickSand text-2xl">{information}</p>
         {link ? (
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <div className="w-full min-h-screen">
+      <div className="w-full min-h-screen max-w-full overflow-x-hidden">
         <NavBar />
         <div className="w-full h-max-content mb-8">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
@@ -28,8 +28,10 @@ const Home: NextPage = () => {
               fillOpacity="1"
               d="M0,288L26.7,240C53.3,192,107,96,160,74.7C213.3,53,267,107,320,149.3C373.3,192,427,224,480,213.3C533.3,203,587,149,640,149.3C693.3,149,747,203,800,192C853.3,181,907,107,960,80C1013.3,53,1067,75,1120,122.7C1173.3,171,1227,245,1280,245.3C1333.3,245,1387,171,1413,133.3L1440,96L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"
             ></path>
-          </svg>{" "}
+          </svg>
         </div>
+
+        <p className="text-center">Descubramos juntos como funciona</p>
         <FeaturesAppDescriptions
           cardContent={{
             reverse: true,
