c7cf565451849304bc251bf1313ecfa8ee8010e9
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-09-28 09:40:08 -0500

    Feature(helpers):Routes definition

---
+++ helpers/rutes.ts
@@ -0,0 +1,13 @@
+type Rutes = {
+  home: string;
+  login: string;
+  principalPage: string;
+  newAcount: string;
+};
+
+export const rutes: Rutes = {
+  home: "/",
+  login: "/login",
+  newAcount: "/register",
+  principalPage: "/home",
+};
