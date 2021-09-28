52f2cc225306dff76a97533fb9c10baa56786be9
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-09-28 10:51:27 -0500

    Feature(global):Add global toast container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { useStore } from "../store";
 import firebaseApp from "../firebase/firebaseApp";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const store = useStore(pageProps.initialReduxState);
   firebaseApp();
   return (
     <Provider store={store}>
+      <ToastContainer />
       <Component {...pageProps} />
     </Provider>
   );
