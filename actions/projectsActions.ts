d6a0c5c3b6e7bac7e9a0cddcc2b9bbb352829f52
Yosimar Sinisterra <michaelyosimar43@gmail.com>
2021-10-07 21:15:22 -0500

    Feature(redux-projects): Add action to notes

--- actions/projectsActions.ts
+++ actions/projectsActions.ts
@@ -42,6 +42,71 @@ const agregarProjecto = (project: object) => {
   project.id = `${uuidv4()}`;
   project.tasks = [];
   project.notes = [];
-
   return { type: AGREGAR_PROYECTO, payload: project };
 };
+
+export const setActualProjectAction = (project: object) => {
+  return (dipatch: any) => dipatch(setActualProject(project));
+};
+
+const setActualProject = (project: object) => ({
+  type: PROYECTO_ACTUAL,
+  payload: project,
+});
+
+export const deleteProjectByIdAction = (projectId: string) => {
+  return (dispatch: any) => dispatch(eliminarProyecto(projectId));
+};
+
+const eliminarProyecto = (proyectId: string) => ({
+  type: ELIMINAR_PROYECTO,
+  payload: proyectId,
+});
+
+export const addNewNoteAction = (note: object) => {
+  return (dispatch: any) => dispatch(agregarNota(note));
+};
+
+const agregarNota = (nota: object) => {
+  const id = String(uuidv4());
+  let f = new Date();
+  const date =
+    f.getDate() +
+    "/" +
+    (f.getMonth() + 1) +
+    "/" +
+    f.getFullYear() +
+    "-" +
+    f.getHours() +
+    ":" +
+    f.getMinutes();
+  return {
+    type: AGREGAR_NOTA,
+    payload: {
+      ...nota,
+      id,
+      date,
+    },
+  };
+};
+
+export const deleteNoteAction = (noteID: string) => {
+  return (dispatch: any) => dispatch(eliminarNota(noteID))
+
+
+}
+
+const eliminarNota = (notaID: string) =>
+({
+  type: ELIMINAR_NOTA,
+  payload: notaID
+})
+
+export const searchNotesAction = (textToSearch: string) => {
+  return (dispatch: any) => dispatch(buscarNotas(textToSearch))
+
+}
+
+const buscarNotas = (texto: string) =>
+  ({ type: BUSQUEDA, payload: texto })
+
